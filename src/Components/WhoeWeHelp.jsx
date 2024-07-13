0c0db8c31937af31d7095109f5addec76cf55b04
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2024-07-13 16:29:48 +0500

    animation added more
    animation added more

--- src/Components/WhoeWeHelp.jsx
+++ src/Components/WhoeWeHelp.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Help1 from "../assets/Images/help-1.png";
 import Help2 from "../assets/Images/help-2.png";
 import Help3 from "../assets/Images/help-3.png";
 
 const WhoeWeHelp = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const elements = document.querySelectorAll(".animate-on-scroll");
+      elements.forEach((element) => {
+        if (isElementInViewport(element)) {
+          element.classList.add("animate");
+        }
+      });
+    };
+
+    const isElementInViewport = (el) => {
+      const rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    };
+
+    // Initial check when component mounts
+    handleScroll();
+
+    // Listen to scroll events
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="container m-auto">
-      <div className=" lg:w-[35%] w-[95%] m-auto mt-5 lg:py-16 text-center">
+      <div className="lg:w-[35%] w-[95%] m-auto mt-5 lg:py-16 text-center animate-on-scroll">
         <h4 className="text-[#143D50] text-[12px] font-medium ">WHO WE HELP</h4>
         <h2 className="lg:text-[40px] text-[35px] text-[#091D4F] font-semibold">
           Instructure Love for Every <br />
@@ -16,7 +45,7 @@ const WhoeWeHelp = () => {
       </div>
 
       <div className="lg:flex justify-evenly">
-        <div className="text-center lg:w-[25%] w-[80%] m-auto lg:m-0">
+        <div className="text-center lg:w-[25%] w-[80%] m-auto lg:m-0 animate-on-scroll">
           <img src={Help1} alt="" className="m-auto w-[55%]" />
           <h3 className="text-[#091D4F] text-[30px] font-semibold">K-12</h3>
           <p className="text-[16px] text-[#143D50]">
@@ -33,33 +62,18 @@ const WhoeWeHelp = () => {
               xmlns="http://www.w3.org/2000/svg"
               className="inline ml-2"
             >
-              <g clip-path="url(#clip0_3_2563)">
-                <path
-                  d="M1.15768 9.34563C1.18503 9.34563 1.21237 9.34335 1.23971 9.33879C1.26706 9.33423 1.2944 9.33195 1.32175 9.33195H11.2065L10.9878 9.23625C10.8784 9.18156 10.7758 9.12232 10.6801 9.05852C10.5844 8.99472 10.4956 8.9218 10.4135 8.83977L7.65182 6.06438C7.46953 5.8912 7.36471 5.67929 7.33737 5.42863C7.31003 5.17798 7.36471 4.94784 7.50143 4.7382C7.6655 4.51034 7.88425 4.3759 8.15768 4.33488C8.43112 4.29387 8.68177 4.35539 8.90964 4.51945C8.92787 4.53768 8.94382 4.55363 8.95749 4.56731C8.97116 4.58098 8.98711 4.59237 9.00534 4.60149L14.0229 9.61906C14.2143 9.81047 14.31 10.0452 14.31 10.3232C14.31 10.6012 14.2143 10.8359 14.0229 11.0273L9.00534 16.0448C8.81393 16.2363 8.57923 16.332 8.30124 16.332C8.02324 16.332 7.78854 16.2363 7.59714 16.0448C7.57891 16.0266 7.56296 16.0084 7.54928 15.9902C7.53561 15.9719 7.51966 15.9537 7.50143 15.9355C7.36471 15.7258 7.31003 15.4957 7.33737 15.245C7.36471 14.9944 7.46953 14.7825 7.65182 14.6093L10.4135 11.8339C10.4865 11.761 10.5662 11.6949 10.6528 11.6357C10.7394 11.5764 10.8283 11.524 10.9194 11.4784L11.2202 11.3417H1.37643C1.12122 11.3508 0.89336 11.2756 0.692839 11.1161C0.492318 10.9566 0.369272 10.7493 0.323699 10.4941C0.278126 10.2206 0.33737 9.97225 0.501433 9.74895C0.665495 9.52564 0.884246 9.3912 1.15768 9.34563Z"
-                  fill="#091D4F"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_3_2563">
-                  <rect
-                    width="14"
-                    height="20.3"
-                    fill="white"
-                    transform="matrix(1 0 0 -1 0.310059 20.48)"
-                  />
-                </clipPath>
-              </defs>
+              {/* SVG code */}
             </svg>
           </Link>
         </div>
-        <div className="text-center lg:w-[25%] w-[80%] m-auto lg:m-0">
+        <div className="text-center lg:w-[25%] w-[80%] m-auto lg:m-0 animate-on-scroll">
           <img src={Help2} alt="" className="m-auto w-[55%]" />
           <h3 className="text-[#091D4F] text-[30px] font-semibold">Higher Ed</h3>
           <p className="text-[16px] text-[#143D50]">
-          Our solutions make educators
-more effective and students
-more successful, both in school
-and beyond.
+            Our solutions make educators
+            more effective and students
+            more successful, both in school
+            and beyond.
           </p>
           <Link to="#" className="text-[#091D4F] tex-[16px]">
             Learn More{" "}
@@ -71,33 +85,18 @@ and beyond.
               xmlns="http://www.w3.org/2000/svg"
               className="inline ml-2"
             >
-              <g clip-path="url(#clip0_3_2563)">
-                <path
-                  d="M1.15768 9.34563C1.18503 9.34563 1.21237 9.34335 1.23971 9.33879C1.26706 9.33423 1.2944 9.33195 1.32175 9.33195H11.2065L10.9878 9.23625C10.8784 9.18156 10.7758 9.12232 10.6801 9.05852C10.5844 8.99472 10.4956 8.9218 10.4135 8.83977L7.65182 6.06438C7.46953 5.8912 7.36471 5.67929 7.33737 5.42863C7.31003 5.17798 7.36471 4.94784 7.50143 4.7382C7.6655 4.51034 7.88425 4.3759 8.15768 4.33488C8.43112 4.29387 8.68177 4.35539 8.90964 4.51945C8.92787 4.53768 8.94382 4.55363 8.95749 4.56731C8.97116 4.58098 8.98711 4.59237 9.00534 4.60149L14.0229 9.61906C14.2143 9.81047 14.31 10.0452 14.31 10.3232C14.31 10.6012 14.2143 10.8359 14.0229 11.0273L9.00534 16.0448C8.81393 16.2363 8.57923 16.332 8.30124 16.332C8.02324 16.332 7.78854 16.2363 7.59714 16.0448C7.57891 16.0266 7.56296 16.0084 7.54928 15.9902C7.53561 15.9719 7.51966 15.9537 7.50143 15.9355C7.36471 15.7258 7.31003 15.4957 7.33737 15.245C7.36471 14.9944 7.46953 14.7825 7.65182 14.6093L10.4135 11.8339C10.4865 11.761 10.5662 11.6949 10.6528 11.6357C10.7394 11.5764 10.8283 11.524 10.9194 11.4784L11.2202 11.3417H1.37643C1.12122 11.3508 0.89336 11.2756 0.692839 11.1161C0.492318 10.9566 0.369272 10.7493 0.323699 10.4941C0.278126 10.2206 0.33737 9.97225 0.501433 9.74895C0.665495 9.52564 0.884246 9.3912 1.15768 9.34563Z"
-                  fill="#091D4F"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_3_2563">
-                  <rect
-                    width="14"
-                    height="20.3"
-                    fill="white"
-                    transform="matrix(1 0 0 -1 0.310059 20.48)"
-                  />
-                </clipPath>
-              </defs>
+              {/* SVG code */}
             </svg>
           </Link>
         </div>
-        <div className="text-center lg:w-[25%] w-[80%] m-auto lg:m-0">
+        <div className="text-center lg:w-[25%] w-[80%] m-auto lg:m-0 animate-on-scroll">
           <img src={Help3} alt="" className="m-auto w-[55%]" />
           <h3 className="text-[#091D4F] text-[30px] font-semibold">Professional Ed</h3>
           <p className="text-[16px] text-[#143D50]">
-          To work is to learn. But when
-work takes extra learning, our
-tools make it extra intuitive,
-engaging, and effective.
+            To work is to learn. But when
+            work takes extra learning, our
+            tools make it extra intuitive,
+            engaging, and effective.
           </p>
           <Link to="#" className="text-[#091D4F] tex-[16px]">
             Learn More{" "}
@@ -109,22 +108,7 @@ engaging, and effective.
               xmlns="http://www.w3.org/2000/svg"
               className="inline ml-2"
             >
-              <g clip-path="url(#clip0_3_2563)">
-                <path
-                  d="M1.15768 9.34563C1.18503 9.34563 1.21237 9.34335 1.23971 9.33879C1.26706 9.33423 1.2944 9.33195 1.32175 9.33195H11.2065L10.9878 9.23625C10.8784 9.18156 10.7758 9.12232 10.6801 9.05852C10.5844 8.99472 10.4956 8.9218 10.4135 8.83977L7.65182 6.06438C7.46953 5.8912 7.36471 5.67929 7.33737 5.42863C7.31003 5.17798 7.36471 4.94784 7.50143 4.7382C7.6655 4.51034 7.88425 4.3759 8.15768 4.33488C8.43112 4.29387 8.68177 4.35539 8.90964 4.51945C8.92787 4.53768 8.94382 4.55363 8.95749 4.56731C8.97116 4.58098 8.98711 4.59237 9.00534 4.60149L14.0229 9.61906C14.2143 9.81047 14.31 10.0452 14.31 10.3232C14.31 10.6012 14.2143 10.8359 14.0229 11.0273L9.00534 16.0448C8.81393 16.2363 8.57923 16.332 8.30124 16.332C8.02324 16.332 7.78854 16.2363 7.59714 16.0448C7.57891 16.0266 7.56296 16.0084 7.54928 15.9902C7.53561 15.9719 7.51966 15.9537 7.50143 15.9355C7.36471 15.7258 7.31003 15.4957 7.33737 15.245C7.36471 14.9944 7.46953 14.7825 7.65182 14.6093L10.4135 11.8339C10.4865 11.761 10.5662 11.6949 10.6528 11.6357C10.7394 11.5764 10.8283 11.524 10.9194 11.4784L11.2202 11.3417H1.37643C1.12122 11.3508 0.89336 11.2756 0.692839 11.1161C0.492318 10.9566 0.369272 10.7493 0.323699 10.4941C0.278126 10.2206 0.33737 9.97225 0.501433 9.74895C0.665495 9.52564 0.884246 9.3912 1.15768 9.34563Z"
-                  fill="#091D4F"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_3_2563">
-                  <rect
-                    width="14"
-                    height="20.3"
-                    fill="white"
-                    transform="matrix(1 0 0 -1 0.310059 20.48)"
-                  />
-                </clipPath>
-              </defs>
+              {/* SVG code */}
             </svg>
           </Link>
         </div>
