8d399a5bca90f73571dca6e884554a65f058e25d
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2024-07-17 21:48:55 +0500

    up
    up

--- src/Page/SideBar.jsx
+++ src/Page/SideBar.jsx
@@ -63,11 +63,9 @@ const Sidebar = ({ collapsed }) => {
       } bg-[#162958] p-4  z-10 h-[100%] flex flex-col transition-transform duration-300 ease-in-out transform ${
         sidebarOpen ? "translate-x-0" : "-translate-x-full lg:translate-x-0"
       } lg:static lg:flex lg:flex-col lg:w-[100px] `}
-    style={{position:"fixed"}}
+      style={{ position: "fixed" }}
     >
-      <div className="flex flex-col items-center mb-4">
-     
-      </div>
+      <div className="flex flex-col items-center mb-4"></div>
       <div className="flex-1">
         <ul className="space-y-1 ">
           <li className="text-primary flex items-center gap-4 p-2 rounded-md ">
@@ -142,24 +140,32 @@ const Sidebar = ({ collapsed }) => {
           </li>
 
           <li>
-          <div className="flex justify-center mt-4">
-        <button
-          onClick={handleLogout}
-          className="lg:inline block font-semibold m-2 text-white p-2 w-[120px] h-fit text-center"
-        >
-          <div className="m-auto text-center flex justify-center">
-          <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M2 18C1.45 18 0.979333 17.8043 0.588 17.413C0.196667 17.0217 0.000666667 16.5507 0 16V2C0 1.45 0.196 0.979333 0.588 0.588C0.98 0.196667 1.45067 0.000666667 2 0H9V2H2V16H9V18H2ZM13 14L11.625 12.55L14.175 10H6V8H14.175L11.625 5.45L13 4L18 9L13 14Z" fill="white"/>
-</svg>
-
-          </div>
-          Logout
-        </button>
-      </div>
+            <div className="flex justify-center mt-4">
+              <button
+                onClick={handleLogout}
+                className="lg:inline block font-semibold m-2 text-white p-2 w-[120px] h-fit text-center"
+              >
+                <div className="m-auto text-center flex justify-center">
+                  <svg
+                    width="18"
+                    height="18"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M2 18C1.45 18 0.979333 17.8043 0.588 17.413C0.196667 17.0217 0.000666667 16.5507 0 16V2C0 1.45 0.196 0.979333 0.588 0.588C0.98 0.196667 1.45067 0.000666667 2 0H9V2H2V16H9V18H2ZM13 14L11.625 12.55L14.175 10H6V8H14.175L11.625 5.45L13 4L18 9L13 14Z"
+                      fill="white"
+                    />
+                  </svg>
+                </div>
+                Logout
+              </button>
+            </div>
           </li>
         </ul>
       </div>
-    
+
       {/* Toggle button for mobile */}
       <button
         className="sidebar-toggle lg:hidden fixed top-4 right-4 z-50 bg-blue-900 text-white p-2 rounded-full"
